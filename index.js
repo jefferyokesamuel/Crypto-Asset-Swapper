ef69b15e2a37a4a48e06bd550f5905b7e3ad67d1
Jeffery Oke-Samuel <31242671+jefferyokesamuel@users.noreply.github.com>
2023-02-11 16:42:54 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 async function connect() {
             console.log("Connecting")
             await ethereum.request({ method: "eth_requestAccounts"})
+        } catch (err) {
+            console.log(err)
+        }
+        
     }
 }
